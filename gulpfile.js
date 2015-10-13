4065338013d0a5efdaa91c9c34f7e0563c15e969
Tyson Sorensen <tysonsorensen3@gmail.com>
2015-10-13 09:29:26 -0600

    added tasks and watchers to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp        = require('gulp');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var sourcemaps  = require('gulp-sourcemaps');
+var htmlmin     = require('gulp-htmlmin');
+var express     = require('express');
 
 gulp.task('default', [
   'scripts',
@@ -12,23 +14,59 @@ gulp.task('default', [
   'templates'
 ]);
 
+gulp.task('watch', [
+  'scripts.watch',
+  'styles.watch',
+  'static.watch',
+  'templates.watch'
+]);
+
 gulp.task('scripts', function() {
   return gulp.src('src/scripts/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('app.min.js'))
+    .pipe(uglify())
     .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('build/js'));
 });
+//watcher
+gulp.task('scripts.watch', ['scripts'], function() {
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+});
 
 gulp.task('styles', function() {
-  console.log('ran styles');
+  return gulp.src('src/styles/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('build/css'));
+});
+//watcher
+gulp.task('styles.watch', ['styles'], function() {
+  gulp.watch('src/styles/**/*.css', ['scripts']);
 });
 
 gulp.task('static', function() {
-  console.log('ran static');
+  return gulp.src('src/static/**')
+    .pipe(gulp.dest('build'));
+});
+//watcher
+gulp.task('static.watch', ['static'], function(){
+  gulp.watch('src/static/**', ['static']);
 });
 
 gulp.task('templates', function() {
   return gulp.src('src/templates/**/*.html')
-    .pipe(gulp.dest('build/templates'));
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'));
+});
+//watcher
+gulp.task('templates.watch', ['templates'], function(){
+  gulp.watch('src/templates/**/*.html', ['templates']);
+});
+
+gulp.task('server', function() {
+  var app = express();
+  app.use(express.static('build'));
+  app.listen(8000);
 });
