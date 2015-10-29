dd5075da3ee204785a3146a8c998787eb7706fcf
Tyson Sorensen <tysonsorensen3@gmail.com>
2015-10-29 08:50:05 -0600

    added routing in angular

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var gulp        = require('gulp');
-var concat      = require('gulp-concat');
-var uglify      = require('gulp-uglify');
-var sourcemaps  = require('gulp-sourcemaps');
-var htmlmin     = require('gulp-htmlmin');
-var express     = require('express');
-var livereload  = require('gulp-livereload');
-var please      = require('gulp-pleeease');
-var lib         = require('bower-files')({
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var htmlmin = require('gulp-htmlmin');
+var express = require('express');
+var livereload = require('gulp-livereload');
+var please = require('gulp-pleeease');
+var lib = require('bower-files')({
   overrides: {
     bootstrap: {
       main: [
@@ -20,19 +20,18 @@ var lib         = require('bower-files')({
   }
 });
 
-
 gulp.task('default', [
+  'fonts',
   'scripts',
   'styles',
-  'fonts',
   'static',
   'templates'
 ]);
 
 gulp.task('watch', [
+  'fonts',
   'scripts.watch',
   'styles.watch',
-  'fonts',
   'static.watch',
   'templates.watch',
   'server',
@@ -40,75 +39,72 @@ gulp.task('watch', [
 ]);
 
 
-gulp.task('scripts', function() {
-  return gulp.src(
-      lib.ext('js').files.concat('src/scripts/**/*.js')
-    )
+gulp.task('scripts', function () {
+  return gulp.src(lib.ext('js').files.concat('src/scripts/**/*.js'))
     .pipe(sourcemaps.init())
-    .pipe(concat('app.min.js'))
-    .pipe(uglify())
+      .pipe(concat('app.min.js'))
+      .pipe(uglify())
     .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('build/js'));
 });
 //watcher
-gulp.task('scripts.watch', ['scripts'], function() {
+gulp.task('scripts.watch', ['scripts'], function () {
   gulp.watch('src/scripts/**/*.js', ['scripts']);
 });
 
 
-gulp.task('styles', function() {
-  return gulp.src(
-      lib.ext('css').files
-      .concat('src/styles/**/*.css')
-    )
+gulp.task('styles', function () {
+  return gulp.src(lib.ext('css').files.concat('src/styles/**/*.css'))
     .pipe(sourcemaps.init())
-    .pipe(concat('app.min.js'))
-    .pipe(please())
+      .pipe(concat('app.min.css'))
+      .pipe(please())
     .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('build/css'));
 });
 //watcher
-gulp.task('styles.watch', ['styles'], function() {
-  gulp.watch('src/styles/**/*.css', ['scripts']);
+gulp.task('styles.watch', ['styles'], function () {
+  gulp.watch('src/styles/**/*.css', ['styles']);
 });
 
 
-gulp.task('fonts', function() {
+gulp.task('static', function () {
+  return gulp.src('src/static/**/*')
+    .pipe(gulp.dest('build'));
+});
+//watcher
+gulp.task('static.watch', ['static'], function () {
+  gulp.watch('src/static/**/*', ['static']);
+});
+
+gulp.task('fonts', function () {
   return gulp.src(lib.ext(['eot', 'svg', 'ttf', 'woff']).files)
     .pipe(gulp.dest('build/fonts'));
 });
 
 
-gulp.task('static', function() {
-  return gulp.src('src/static/**')
-    .pipe(gulp.dest('build'));
-});
-//watcher
-gulp.task('static.watch', ['static'], function(){
-  gulp.watch('src/static/**', ['static']);
-});
-
-
-gulp.task('templates', function() {
+gulp.task('templates', function () {
   return gulp.src('src/templates/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     .pipe(gulp.dest('build'));
 });
 //watcher
-gulp.task('templates.watch', ['templates'], function(){
+gulp.task('templates.watch', ['templates'], function () {
   gulp.watch('src/templates/**/*.html', ['templates']);
 });
 
-
-gulp.task('server', function() {
+//startup server on localhost
+gulp.task('server', function () {
   var app = express();
   app.use(express.static('build'));
   app.listen(8000);
 });
 
-gulp.task('livereload', function(){
-  var server = livereload.listen();
-  gulp.watch('build/**', function(event) {
+
+gulp.task('livereload', function () {
+  livereload.listen();
+  gulp.watch('build/**/*', function (event) {
     livereload.changed(event);
   });
 });
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,16 @@
-console.log("hello app.js");
+angular
+  .module('TodoApp', [
+    'ngRoute',
+    'LoginController'
+  ])
+  .config([
+    '$routeProvider',
+    function($routeProvider) {
+      $routeProvider
+        .when('/login', {
+          templateUrl: '/partials/login-controller.html',
+          controller: 'LoginController'
+        })
+        .otherwise('/login');
+    }
+  ]);
